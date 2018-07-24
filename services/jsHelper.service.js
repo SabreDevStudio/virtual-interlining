80fa983c7729aedfb7de190d29095db72f01897e
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-07-24 14:58:04 +0200

    html to json done

--- services/jsHelper.service.js
+++ services/jsHelper.service.js
@@ -1,4 +1,6 @@
 const colorCodes = require('./colorCodes.service')
+var fs = require('fs');
+var easyconvert = require('easyconvert');
 
 const jsHelper = {
   toOneLineString: structure => {
@@ -8,10 +10,12 @@ const jsHelper = {
     }
     return oneLineString
   },
+
   getMMPList: dom => {
     let MMPLIST = Array.prototype.slice.call(dom.window.document.querySelectorAll('table.fieldlist table.fieldlist table.fieldlist'))
     return MMPLIST.filter(el => el.outerHTML.includes('MML') ? el : null)
   },
+  
   argumentsValidation: (args, cb) => {
     if (args.length !== 5) {
       console.log(`${colorCodes.BGgreen}${colorCodes.red}
@@ -22,6 +26,59 @@ const jsHelper = {
     } else {
       cb(false)
     }
+  },
+
+  deepMap: (obj, f) => {
+    return Object.keys(obj).reduce(function(acc, k) {
+      if ({}.toString.call(obj[k]) == '[object Object]') {
+        acc[k] = jsHelper.deepMap(obj[k], f)
+      } else {
+        acc[k] = f(obj[k], k)
+      }
+      return acc
+    },{})
+  },
+
+  joinObjects: arrObj => {
+    let counter = 0
+    return arrObj.reduce((acc, x, index) => {
+      if((index) % 6 === 0) {
+        counter++
+        acc[counter] = {}
+      }
+  
+      for (var key in x) acc[counter][key] = x[key]
+      return acc
+    }, {})
+  },
+
+  logIt: data => {
+    fs.writeFile('log.json', JSON.stringify(data), 'utf8', () => {});
+  },
+
+  getMmlList: list => {
+    let MMLLIST = []
+  
+    list.forEach(element => {
+      let mmlItem = easyconvert.parse(element.outerHTML)
+      let mmlTags = []
+  
+      jsHelper.deepMap(mmlItem, (value, key) => {
+        if (key === 'innerHTML' && value !== '\n' && value !== 'MML') {
+          mmlTags.push(value.replace(':', ''))
+        }
+      })
+  
+      MMLLIST.push(jsHelper.joinObjects(mmlTags.map((el, index) => {
+        if (index % 2 === 0) {
+          let newObj = {}
+          newObj[el] = mmlTags[index += 1]
+          return newObj
+        }
+      }).filter((el) => el ? el : null)))
+    })
+  
+    return MMLLIST
   }
 }
 
